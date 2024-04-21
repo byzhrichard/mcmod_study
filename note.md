3a4903f2d1d7250b5c069f116e15d3d0175eaac5
byzh <805278087@qq.com>
2024-04-21 21:15:24 +0800

    awa

--- note.md
+++ note.md
@@ -1,1 +1,8 @@
-gradle.properties放着配置信息
+# gradle.properties放着配置信息
+mod_version=1.0.0
+maven_group=com.byzh
+archives_base_name=try-mod
+
+# src/main/resources/fabric.mod.json放着更详细的配置信息
+license协议
+icon模组图片
---
+++ src/main/java/com/byzh/TryModClient.java
@@ -0,0 +1,10 @@
+package com.byzh;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TryModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
