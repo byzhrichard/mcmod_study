09e1bbf3c077d6527970b835a430b2b88dc4ff8c
byzh <805278087@qq.com>
2024-04-23 19:23:14 +0800

    awa

---
+++ src/main/java/com/byzh/item/custom/Prospector.java
@@ -0,0 +1,54 @@
+package com.byzh.item.custom;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockStateRaycastContext;
+import net.minecraft.world.World;
+
+public class Prospector extends Item {
+    public Prospector(Settings settings) {
+        super(settings);
+    }
+
+    @Override
+    public ActionResult useOnBlock(ItemUsageContext context) {
+        World world = context.getWorld();
+        if (!world.isClient()){ //客户端与服务端的判断
+            BlockPos blockPos = context.getBlockPos();
+            PlayerEntity player = context.getPlayer();
+            boolean foundBlock = false;
+            for (int i = -64; i <= blockPos.getY()+64; i++) {
+                BlockState state = context.getWorld().getBlockState(blockPos.down(i));
+                if (isRightBlock(state)){
+                    outputMessage(blockPos.down(i), player, state.getBlock());
+                    foundBlock= true;
+                    break;
+                }
+            }
+            if (!foundBlock){
+                player.sendMessage(Text.literal("No Ore Found!"));
+            }
+        }
+        //减去耐久值
+        context.getStack().damage(1,context.getPlayer(),
+                playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
+        return ActionResult.SUCCESS;
+    }
+
+    private void outputMessage(BlockPos down, PlayerEntity player, Block block) {
+        player.sendMessage(Text.literal("Found"+block.asItem().getName().getString() + "at" +
+                "(" + down.getX() + "," + down.getY() + "," + down.getZ() + ")!"),false);
+    }
+
+    private boolean isRightBlock(BlockState state){
+        return state.isOf(Blocks.IRON_ORE);
+
+    }
+}
