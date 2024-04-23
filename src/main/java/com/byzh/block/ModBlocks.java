69664b48fce093794a7bafb22b4fc14ba1fa87b8
byzh <805278087@qq.com>
2024-04-23 16:23:53 +0800

    awa

--- src/main/java/com/byzh/block/ModBlocks.java
+++ src/main/java/com/byzh/block/ModBlocks.java
@@ -5,13 +5,18 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
+//    public static final Block ICE_ETHER_BLOCK_ORE = registerBlocks(
+//            "ice_ether_block_ore",
+//            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.STONE), UniformIntProvider.create(2, 5)));
     public static final Block ICE_ETHER_BLOCK = registerBlocks(
             "ice_ether_block",
             new Block(FabricBlockSettings.copyOf(Blocks.STONE)));
