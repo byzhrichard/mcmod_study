bba671bf22d1805f9888965a918c4f5f53aef446
byzh <805278087@qq.com>
2024-04-24 15:59:25 +0800

    awa

--- src/main/java/com/byzh/item/ModFoodComponents.java
+++ src/main/java/com/byzh/item/ModFoodComponents.java
@@ -1,10 +1,8 @@
 package com.byzh.item;
 
-import net.minecraft.entity.effect.StatusEffect;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
-import net.minecraft.item.FoodComponents;
 
 public class ModFoodComponents {
     public static final FoodComponent CORN = new FoodComponent.Builder()
