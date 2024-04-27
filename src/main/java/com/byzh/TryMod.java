f5d56aa6ad105d5b83373c8dc78f00c02795a2d4
byzh <805278087@qq.com>
2024-04-27 13:38:51 +0800

    awa

--- src/main/java/com/byzh/TryMod.java
+++ src/main/java/com/byzh/TryMod.java
@@ -4,6 +4,7 @@ import com.byzh.block.ModBlocks;
 import com.byzh.item.ModItemGroup;
 import com.byzh.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+//import net.fabricmc.loader.api.FabricLoader;
 
 import net.fabricmc.fabric.api.registry.FuelRegistry;
 import org.slf4j.Logger;
@@ -12,7 +13,10 @@ import org.slf4j.LoggerFactory;
 public class TryMod implements ModInitializer {
 	public static final String MOD_ID = "try-mod";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+//	FabricLoader loader = FabricLoader.getInstance();
+//
+//	// 获取游戏配置目录
+//	java.nio.file.Path configDir = loader.getGameDirectory().resolve("config");
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
--- src/main/java/com/byzh/item/custom/Prospector.java
+++ src/main/java/com/byzh/item/custom/Prospector.java
@@ -12,6 +12,14 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.BlockStateRaycastContext;
 import net.minecraft.world.World;
+import net.fabricmc.loader.api.FabricLoader;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Random;
 
 public class Prospector extends Item {
     //构造函数
@@ -38,10 +46,25 @@ public class Prospector extends Item {
             if (!foundBlock){
                 player.sendMessage(Text.literal("No Ore Found!"));
             }
+            //test
+            FabricLoader loader = FabricLoader.getInstance();
+            java.nio.file.Path configDir = loader.getGameDir();
+            String path = configDir.toString()+"\\config\\byzh";
+            File newDirectory = new File(path);
+            newDirectory.mkdir();
+            int FileCnt = newDirectory.list().length;
+            try {
+                mytry(path, FileCnt);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            player.sendMessage(Text.literal(path),false);
         }
         //减去耐久值
         context.getStack().damage(1, context.getPlayer(),
                 playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
+
+
         return ActionResult.SUCCESS;
     }
 
@@ -53,5 +76,34 @@ public class Prospector extends Item {
     private boolean isRightBlock(BlockState state){
 //        return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DIAMOND_ORE);
         return state.isIn(ModTags.Blocks.PROSPECTOR_LIST);
+    }
+
+    private void mytry(String path,int FileCnt) throws IOException {
+
+        int width = 800; // 画布宽度
+        int height = 600; // 画布高度
+        int[][] pixels = new int[width][height]; // 初始化画布
+
+        Random random = new Random();
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                int red = random.nextInt(256); // 生成0-255的随机数作为红色分量
+                int green = random.nextInt(256); // 生成0-255的随机数作为绿色分量
+                int blue = random.nextInt(256); // 生成0-255的随机数作为蓝色分量
+
+                int pixel = (red << 16) | (green << 8) | blue; // 合并RGB值
+                pixels[x][y] = pixel; // 将像素值存入画布中
+            }
+        }
+
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                image.setRGB(x, y, pixels[x][y]); // 设置像素颜色
+            }
+        }
+
+        File output = new File(path+"\\output"+ FileCnt +".png");
+        ImageIO.write(image, "png", output); // 保存为PNG格式的图片
     }
 }
